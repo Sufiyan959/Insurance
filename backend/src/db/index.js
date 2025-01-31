4db72749d9fa4c63a5508bc049018e5e6b769f00
Yashwanth-Chary-P <p.yashwanthchary@gmail.com>
2025-01-31 17:12:21 +0530

    Adding backend models and middlewares

---
+++ backend/src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+import path from "path"; // ✅ Import path
+
+dotenv.config({ path: path.resolve(process.cwd(), "src/.env") }); // ✅ Adjusted path
+
+const mongoUrl = "mongodb+srv://dbyashwanth:dbyashwanth@books-store-mern.7jdpz.mongodb.net/insurance?retryWrites=true&w=majority&appName=Books-Store-MERN"
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://dbyashwanth:dbyashwanth@books-store-mern.7jdpz.mongodb.net/insurance?retryWrites=true&w=majority&appName=Books-Store-MERN");
+        console.log(`MongoDB connected!`);
+    } catch (error) {
+        console.log("MongoDB Connection error", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
